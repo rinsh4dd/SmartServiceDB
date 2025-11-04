8d8174f5ccf9353f256fe03280370ac56060260d
rinsh4dd <rinshadcontacts@gmail.com>
2025-11-04 10:08:17 +0530

    DataBase Updated on 04-11-2025 10:08:17.87

--- daily_log.bat
+++ daily_log.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "C:\Users\rinsh\OneDrive\Documents\SQL Server Management Studio\SmartServeDB"
-echo Auto commit at %date% %time% >> DatabaseUpdates.txt
+echo Database Updated at %date% %time% >> DatabaseUpdates.txt
 git add .
 git commit -m "DataBase Updated on %date% %time%"
 git push origin main
