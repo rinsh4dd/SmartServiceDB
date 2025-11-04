2117442c1dadda337d829e24ee7a14562e6aa9c9
rinsh4dd <rinshadcontacts@gmail.com>
2025-11-04 09:46:11 +0530

    Technician Service Updated on 04-11-2025  9:46:11.69

--- daily_log.bat
+++ daily_log.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "C:\Users\rinsh\OneDrive\Desktop\vscode\GitAutoLogger\newRepoServices\Implementations"
-echo Auto commit at %date% %time% >> TechnicianServices.txt
+echo Auto commit at %date% %time% >> DatabaseUpdates.txt
 git add .
 git commit -m "Technician Service Updated on %date% %time%"
 git push origin main
--- run_silent.vbs
+++ run_silent.vbs
@@ -1,3 +1,3 @@
 Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run chr(34) & "C:\Users\rinsh\OneDrive\Desktop\vscode\GitAutoLogger\newRepo\daily_log.bat" & chr(34), 0
+WshShell.Run chr(34) & "C:\Users\rinsh\OneDrive\Documents\SQL Server Management Studio\SmartServeDB\daily_log.bat" & chr(34), 0
 Set WshShell = Nothing
